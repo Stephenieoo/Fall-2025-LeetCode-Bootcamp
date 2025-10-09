11d1a30c9d4e93439e713c3d537f05f73b21ca1e
Stephenieoo <xo258@nyu.edu>
2025-10-08 22:21:44 -0400

    add StringtoInteger.py

---
+++ Week 2/Assignment/StringtoInteger.py
@@ -0,0 +1,29 @@
+def myAtoi(self, s: str) -> int:
+    n = len(s)
+    if n == 0:
+        return 0
+    sign = 1
+    i = 0
+    while i < n and s[i] == ' ':
+        i += 1
+    if i < n and s[i] in ['-', '+']:
+        if s[i] == '-':
+            sign = -1
+        i += 1
+    
+    num = 0
+    for j in range(i, n):
+        if s[j].isdigit():
+            print(s[j])
+            num = num * 10 + int(s[j])
+        else:
+            break
+    max_int, min_int = 2**31 - 1, -2**31
+    
+    if sign * num > 2**31 - 1:
+        return 2**31 - 1
+    if sign * num < -2**31:
+        return -2**31
+    return sign * num
+    
+
